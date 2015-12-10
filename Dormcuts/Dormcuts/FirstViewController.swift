c4640f203d625aaf09b431452211ad14d3df306b
simonorlovsky <simon.orlovsky@yahoo.com>
2015-12-09 18:35:34 -0600

    first commit

---
+++ Dormcuts/Dormcuts/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Dormcuts
+//
+//  Created by Simon Orlovsky on 12/9/15.
+//  Copyright © 2015 simonorlovsky. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Dormcuts/DormcutsTests/DormcutsTests.swift
@@ -0,0 +1,36 @@
+//
+//  DormcutsTests.swift
+//  DormcutsTests
+//
+//  Created by Simon Orlovsky on 12/9/15.
+//  Copyright © 2015 simonorlovsky. All rights reserved.
+//
+
+import XCTest
+@testable import Dormcuts
+
+class DormcutsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
